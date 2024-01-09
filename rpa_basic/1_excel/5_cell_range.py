1952e7fac66d6d8dcdb15a56ca27a39511204810
kevinyun31 <kevinyun31@gmail.com>
2024-01-09 09:14:07 +0900

    python file 가져오기_2

--- rpa_basic/1_excel/5_cell_range.py
+++ rpa_basic/1_excel/5_cell_range.py
@@ -69,9 +69,14 @@ for i in range(1, 11): # 10개 데이터 넣기
 # for column in ws.iter_cols(): # 전체 column
 #     print(column[0].value)
 
-# 2번째 줄부터 11번째 줄까지, 2번째 열부터 3번째 열까지
-for row in ws.iter_rows(min_row=2, max_row=11, min_col=2, max_col=3): # 전체 row
-    print(row[0].value, row[1].value) # 수학, 영어
+# row = 데이터를 좌우로 읽어오기, col = 데이터를 세로로 읽어오기
+# 1번째 줄부터 11번째 줄까지, 1번째 열부터 3번째 열까지
+for row in ws.iter_rows(min_row=1, max_row=11, min_col=1, max_col=3): # 전체 row 와 column
+    print(row[0].value, row[1].value, row[2].value) # 번호, 수학, 영어 의 값
+# 2번째 줄부터 5번째 줄까지, 1번째 열부터 3번째 열까지  
+for col in ws.iter_cols(min_row=2, max_row=5, min_col=1, max_col=3): # min or max default 는 0부터
+    print(col)    
+    
     
 wb.save("sample.xlsx")
-print(" *모두 성공 적으로 완성되었습니다* ")
+print(" < 모두 성공 적으로 완성되었습니다 > ")
